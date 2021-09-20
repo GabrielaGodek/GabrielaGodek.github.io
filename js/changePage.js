6826e718eebfe5d4776181d12d5e42e5709c8404
Gabriela Godek <83614025+GabrielaGodek@users.noreply.github.com>
2021-09-20 18:56:23 +0100

    Delete js directory

--- js/changePage.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('.formBtn').click(function () {
-        //przeładowanie strony
-        $('.pageChange').load('./Page2.html').hide().fadeIn(500);
-        //zmiana opacity dla zdjecia w tle
-        $('.imgBack').fadeTo(200, .2);
-    })
-})
-
--- js/changePage3.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.thirdForm').submit(function (e) {
-        e.preventDefault();
-        $('.pageChange').load('./Page4.html').hide().fadeIn(500);
-    });
-})
